e3eb71643d327a4a61c6003bc85d864ed86bd154
BLooDBRothER <computertechnoloy446@gmail.com>
2021-06-14 09:55:34 +0530

    js1: Finish animation Add simple game

--- js1-DrumKit/main.js
+++ js1-DrumKit/main.js
@@ -1,4 +1,10 @@
-
+const txt = document.querySelector(".txt-cnt");
+const point = document.querySelector(".point");
+const keys = document.querySelectorAll(".key");
+let pt = 0;
+let fl = 0;
+point.innerHTML = pt;
+let snd;
 let t = {
     65:"boom",
     66:"tink",
@@ -28,9 +34,51 @@ let t = {
     90:"boom"
 }
 
+keys.forEach(key => {
+    key.addEventListener("transitionend", function(e){
+        key.classList.remove("pressed");
+    })
+});
+
 window.addEventListener("keydown", function(e){
     const aud = document.querySelector(`audio[data-key=${t[e.keyCode]}]`);
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
     if(!aud) return;
+    key.classList.add("pressed");
+    check(t[e.keyCode], snd);
     aud.currentTime = 0;
     aud.play();
 });
+
+function addtag(data){
+    let dt = document.createElement("p");
+    dt.innerHTML = data;
+    txt.appendChild(dt);
+    setTimeout(()=>{
+        remtag(dt);
+    },1800);
+}
+
+function remtag(data){
+    txt.classList.remove("green");
+    fl = 0;
+    snd = "";
+    txt.removeChild(data);
+}
+
+function game(){
+    let rnd = Math.floor(Math.random()*(90-65) + 65);
+    snd = t[rnd];
+    addtag(snd);
+}
+
+function check(key, ori){
+    if(key == ori && fl == 0){
+        point.innerHTML = ++pt;
+        txt.classList.add("green");
+        fl = 1;
+    }
+}
+setInterval(() => {
+    game();
+}, 2000);
