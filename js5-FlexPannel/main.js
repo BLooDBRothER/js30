ae4ada737cd83b72b372f71164a505d10ff403d6
BLooDBRothER <computertechnoloy446@gmail.com>
2021-07-04 16:54:02 +0530

    js5: Add icon

--- js5-FlexPannel/main.js
+++ js5-FlexPannel/main.js
@@ -2,19 +2,15 @@ const panels = document.querySelector(".panels");
 const panel = document.querySelectorAll(".panel");
 const panelP = document.querySelectorAll(".panel > *");
 let i=0, j=0, init=1;
-let autoval = true;
 
 function toggleFlex(){
     this.classList.toggle("open");
     setTimeout(() => {
         this.classList.toggle("open-active");
     }, 700);
-    init = 0;
-    move = true
 }
 
 function changeColor(e){
-    console.log(e);
     let x = e.clientX;
     let y = e.clientY;
     let select = document.elementFromPoint(x, y);
@@ -24,27 +20,27 @@ function changeColor(e){
     select.style.color = `rgb(${red}, ${green}, ${blue})`;
 }
 
-function autoLoopOpen(){
-    panel[i].classList.toggle("open");
-    setTimeout(autoLoopClose, 1500);
-    console.log(panel[i]);
-    i >= 4 ? (i = 0) : i++;
-    console.log(panel[i]); 
-}
+// function autoLoopOpen(){
+//     panel[i].classList.toggle("open");
+//     setTimeout(autoLoopClose, 1500);
+//     console.log(panel[i]);
+//     i >= 4 ? (i = 0) : i++;
+//     console.log(panel[i]); 
+// }
 
-function autoLoopClose(){
-    panel[j].classList.toggle("open");
-    console.log(panel[j]);
-    j >= 4 ? (j = 0) : j++; 
-    console.log(panel[j]);
-}
+// function autoLoopClose(){
+//     panel[j].classList.toggle("open");
+//     console.log(panel[j]);
+//     j >= 4 ? (j = 0) : j++; 
+//     console.log(panel[j]);
+// }
 
-function autoCheck(){
-    if(init){
-        setInterval(autoLoopOpen, 2200);
-        init = 0;
-    }
-}
+// function autoCheck(){
+//     if(init){
+//         setInterval(autoLoopOpen, 2200);
+//         init = 0;
+//     }
+// }
 
 panel.forEach(pan => {
     pan.addEventListener("mouseenter", toggleFlex);
