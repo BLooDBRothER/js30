94cfc862c9656d8dbc88c4919d017a00dd833cad
BLooDBRothER <computertechnology446@gmail.com>
2021-08-20 09:14:13 +0530

    js 15 complete

--- js15-LocalAndEvent/main.js
+++ js15-LocalAndEvent/main.js
@@ -1,3 +1,44 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
-const items = [];
+const items = JSON.parse(localStorage.getItem("items")) || [];
+
+let text;
+
+function updateLocalStorage(){
+    localStorage.setItem("items", JSON.stringify(items));
+}
+
+function addItem(e){
+    e.preventDefault();
+    text = (this.querySelector("input[name='item']")).value; 
+    let item = {
+        text,
+        done: false
+    }
+    items.push(item);
+    populateList(items, itemsList);
+    updateLocalStorage();
+    this.reset();
+}
+
+function populateList(dish = [], plateList){
+    plateList.innerHTML = dish.map((item, i) => {
+        return `
+            <li>
+                <input type="checkbox" data-index=${i} data-item="${item.text}" id="${item.text}" ${item.done ? "checked" : ""}>
+                <label for="${item.text}">${item.text}</label>
+            </li>`
+    }).join("");
+}
+
+function toggleCheck(e){
+    if(!e.target.matches("input")) return;
+    let elem = e.target;
+    items[elem.dataset.index].done = elem.checked;
+    updateLocalStorage();
+}
+
+addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleCheck)
+
+populateList(items, itemsList);
